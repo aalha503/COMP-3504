1bb3d44f8fb4156f1c86928ffcb3aa63c1d90678
Abdullah Alhawsawi <abdullahalhawsawi@Abdullahs-MacBook-Pro.local>
2017-11-12 14:54:07 -0700

    Commit

--- Connect2Fit/app/src/main/java/com/example/muhammadabdullah/connect2fit/TrainerPage.java
+++ Connect2Fit/app/src/main/java/com/example/muhammadabdullah/connect2fit/TrainerPage.java
@@ -1,8 +1,10 @@
 package com.example.muhammadabdullah.connect2fit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -12,18 +14,17 @@ public class TrainerPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_trainer_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+    }
+
+    public void trainerFindonClick(View v){
+        Intent k = new Intent(this, findAtrainer.class);
+        startActivity(k);
+
     }
 
 }
